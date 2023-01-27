579afd46f686621de651c7c95a6a92071e0c13b4
Amrithanshu Kesoju <92728684+amrith-anshu@users.noreply.github.com>
2023-01-27 12:36:26 +0530

    Update Day4ValidParenthesis.py

--- Day4ValidParenthesis.py
+++ Day4ValidParenthesis.py
@@ -2,4 +2,4 @@ class Solution:
     def isValid(self, s: str) -> bool:
         while '()' in s or '[]'in s or '{}' in s:
             s = s.replace('()','').replace('[]','').replace('{}','')
-        return False if l
+        return False if len(s) != 0 else True
