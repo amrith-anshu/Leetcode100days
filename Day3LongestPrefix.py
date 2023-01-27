2d21e41f5521dfe198cd3774eca9b79687152783
Amrithanshu Kesoju <92728684+amrith-anshu@users.noreply.github.com>
2023-01-27 12:16:46 +0530

    Update Day3LongestPrefix.py

--- Day3LongestPrefix.py
+++ Day3LongestPrefix.py
@@ -1,6 +1,7 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         res = ""
+        zip makes the
         for a in zip(*strs):
             if len(set(a)) == 1: 
                 res += a[0]
