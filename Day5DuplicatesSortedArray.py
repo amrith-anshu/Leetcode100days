4aeb05f6163c28458653f2c3efda82ec671c3381
Amrithanshu Kesoju <92728684+amrith-anshu@users.noreply.github.com>
2023-02-13 15:53:39 +0530

    Update Day5DuplicatesSortedArray.py

--- Day5DuplicatesSortedArray.py
+++ Day5DuplicatesSortedArray.py
@@ -1,7 +1,7 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         i=0
-        
+        #search acrossing the list
         for j in nums:
             if j not in nums[:i]:
                 nums[i]=j
